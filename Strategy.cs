fabba4e150755bb1c6150291a55df8ba9dd83a05
salvadorcruz <salvador.cmarin@gmail.com>
2015-02-13 20:59:29 -0800

    Strategy Ejemplos
    Ejemplos de codigo en C# del patron Strategy que expondre en clase.
    Cruz Marin Salvador
    11211246

---
+++ Strategy.cs
@@ -0,0 +1,114 @@
+//Dibuja Catalogo
+using System.Collections.Generic;
+
+public interface DibujaCatalogo
+{
+    void dibuja(IList<VistaVehiculo> contenido);
+}
+
+//Dibuja Tres Vehiculos por Linea
+using System;
+using System.Collections.Generic;
+
+public class DibujaTresVehiculosPorLinea : DibujaCatalogo
+{
+    public void dibuja(IList<VistaVehiculo> contenido)
+    {
+        int contador;
+        Console.WriteLine(
+          "Dibuja los vehículos mostrando tres vehículos por línea");
+        contador = 0;
+        foreach (VistaVehiculo vistaVehiculo in contenido)
+        {
+            vistaVehiculo.dibuja();
+            contador++;
+            if (contador == 3)
+            {
+                Console.WriteLine();
+                contador = 0;
+            }
+            else
+                Console.Write(" ");
+        }
+        if (contador != 0)
+            Console.WriteLine();
+        Console.WriteLine();
+    }
+}
+
+//Dibuja un Vehiculo por Linea
+using System;
+using System.Collections.Generic;
+
+public class DibujaUnVehiculoPorLinea : DibujaCatalogo
+{
+
+    public void dibuja(IList<VistaVehiculo> contenido)
+    {
+        Console.WriteLine(
+          "Dibuja los vehículos mostrando un vehículo por línea");
+        foreach (VistaVehiculo vistaVehiculo in contenido)
+        {
+            vistaVehiculo.dibuja();
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+//Usuario
+public class Usuario
+{
+    static void Main(string[] args)
+    {
+        VistaCatalogo vistaCatalogo1 = new VistaCatalogo(new
+        DibujaTresVehiculosPorLinea());
+        vistaCatalogo1.dibuja();
+        VistaCatalogo vistaCatalogo2 = new VistaCatalogo(new
+        DibujaUnVehiculoPorLinea());
+        vistaCatalogo2.dibuja();
+    }
+}
+
+//Vistas Catalogo
+using System.Collections.Generic;
+
+public class VistaCatalogo
+{
+    protected IList<VistaVehiculo> contenido =
+        new List<VistaVehiculo>();
+    protected DibujaCatalogo dibujo;
+
+    public VistaCatalogo(DibujaCatalogo dibujo)
+    {
+        contenido.Add(new VistaVehiculo("vehículo económico"));
+        contenido.Add(new VistaVehiculo("vehículo amplio"));
+        contenido.Add(new VistaVehiculo("vehículo rápido"));
+        contenido.Add(new VistaVehiculo("vehículo confortable"));
+        contenido.Add(new VistaVehiculo("vehículo deportivo"));
+        this.dibujo = dibujo;
+    }
+
+    public void dibuja()
+    {
+        dibujo.dibuja(contenido);
+    }
+}
+
+//vista Vehiculo
+using System;
+
+public class VistaVehiculo
+{
+    protected string descripcion;
+
+    public VistaVehiculo(string descripcion)
+    {
+        this.descripcion = descripcion;
+    }
+
+    public void dibuja()
+    {
+        Console.Write(descripcion);
+    }
+}
